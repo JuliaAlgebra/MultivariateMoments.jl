52338dc11320cbe2b16a90dd8d3cf26bc19c0029
Benoît Legat <benoit.legat@gmail.com>
2018-06-03 15:42:52 +0200

    Add tests

--- test/atomic.jl
+++ test/atomic.jl
@@ -6,6 +6,7 @@
     @test string(η) == "Atomic measure on the variables x, y with 2 atoms:\n at [1.0, 0.0] with weight 2.0\n at [0.5, 0.5] with weight 3.0"
     p = x^2 + x + y + x*y^2 + 1
     @test dot(η, p) == 2 * p(x=>1, y=>0) + 3 * p(x => 1/2, y => 1/2)
+    @test dot(η, p) == dot(p, η)
     μ = measure(η, monomials(p))
     @test dot(μ, p) == 2 * p(x=>1, y=>0) + 3 * p(x => 1/2, y => 1/2)
 end
