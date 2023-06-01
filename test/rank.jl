66ff267ce4f2ae607b094c8df32870889a03c1a8
Benoît Legat <benoit.legat@gmail.com>
2023-06-01 07:36:38 -0700

    Less fragile test

--- test/rank.jl
+++ test/rank.jl
@@ -1,7 +1,7 @@
 @testset "low_rank_ldlt" begin
     v = [1, -1]
     M = v * v'
-    ldlt = low_rank_ldlt(M, SVDLDLT(), 0)
+    ldlt = low_rank_ldlt(M, SVDLDLT(), 1e-10)
     @test ldlt.L ≈ -normalize(v)
     ldlt = low_rank_ldlt(M, ShiftCholeskyLDLT(1e-10), 1e-9)
     @test ldlt.L ≈ v
