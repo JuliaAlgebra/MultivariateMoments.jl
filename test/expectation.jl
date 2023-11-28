6e2df03415e270e56ae72dded9c8bec224fa09bf
Benoît Legat <benoit.legat@gmail.com>
2023-11-28 16:28:09 +0100

    Improve type stability of expectation

--- test/expectation.jl
+++ test/expectation.jl
@@ -3,10 +3,10 @@
     p = x[3] - 2x[1] * x[2]^2 + 3x[3] * x[1] - 5x[1]^3
     v = (1, 2, 3)
     m = dirac(monomials(p), x => v)
-    @test MultivariateMoments.expectation(m, p) ==
+    @test (@inferred MultivariateMoments.expectation(m, p)) ==
           p(x => v) ==
-          MultivariateMoments.expectation(p, m)
+          (@inferred MultivariateMoments.expectation(p, m))
     @test_throws ErrorException dot(x[1] * x[2] * x[3], m)
-    @test dot(0.5 * x[1] * x[2]^2, m) == 2.0
-    @test dot(m, x[1] * x[3]) == 3
+    @test (@inferred dot(0.5 * x[1] * x[2]^2, m)) == 2.0
+    @test (@inferred dot(m, x[1] * x[3])) == 3
 end
