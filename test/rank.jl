bcc1bf67a6f4233c21e26114db9fa7a2122adfee
Benoît Legat <benoit.legat@gmail.com>
2024-08-02 11:34:47 +0200

    Fix rank warning

--- test/rank.jl
+++ test/rank.jl
@@ -10,3 +10,17 @@
     ldlt = low_rank_ldlt(M, SVDLDLT(), 1e-10)
     @test ldlt.L ≈ -normalize(v)
 end
+
+struct HardcodedRanks <: RankCheck
+    r::Vector{Int}
+end
+
+MultivariateMoments.rank_from_singular_values(σ, r::HardcodedRanks) = r.r[length(σ)]
+
+@testset "Decreasing rank" begin
+    r = RankDependence(zeros(4, 4), HardcodedRanks([1, 0, 1, 2]))
+    @test !is_dependent!(r, 1)
+    @test is_dependent!(r, 2)
+    @test is_dependent!(r, 3)
+    @test is_dependent!(r, 4)
+end
